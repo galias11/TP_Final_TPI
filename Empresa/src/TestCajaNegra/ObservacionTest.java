45e8141b94a65af2ee1c4de04b52fe083fb063d2
Gerardo <alias_gerardo@yahoo.com.ar>
2016-11-25 17:09:26 -0300

    Test Sute caja negra -- Final

--- Empresa/src/TestCajaNegra/ObservacionTest.java
+++ Empresa/src/TestCajaNegra/ObservacionTest.java
@@ -3,6 +3,9 @@ package TestCajaNegra;
 
 import empresa.Observacion;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 import static org.junit.Assert.*;
 
 import org.junit.After;
@@ -17,6 +20,13 @@ public class ObservacionTest {
         super();
     }
     
+    private void resetHoraFecha(Calendar fecha){
+        fecha.set(Calendar.HOUR, 0);
+        fecha.set(Calendar.MINUTE, 0);
+        fecha.set(Calendar.SECOND, 0);
+        fecha.set(Calendar.MILLISECOND, 0);
+    }
+    
     @Before
     public void setUp(){
         fixture.setUp();
@@ -29,7 +39,7 @@ public class ObservacionTest {
     
     
     @Test
-    public void testObs01A(){
+    public void testOBS01A(){
         Observacion aux= new Observacion(Observacion.TEMA_INSUMOS, 1, "Falta material 1");
         assertTrue("No se registro tema correctamente", aux.getTema()==Observacion.TEMA_INSUMOS);
         assertTrue("No se registro legajo correctamente", aux.getNLegCreador()==1);
@@ -37,7 +47,7 @@ public class ObservacionTest {
     }
     
     @Test
-    public void testObs02A(){
+    public void testOBS01B(){
         boolean assertError=false;
         try{
         Observacion aux= new Observacion(Observacion.TEMA_INSUMOS, -1, "Falta material 1");
@@ -50,7 +60,7 @@ public class ObservacionTest {
     }
     
     @Test
-    public void testObs02B(){
+    public void testOBS01C(){
         boolean assertError=false;
         try{
         Observacion aux= new Observacion(Observacion.TEMA_INSUMOS, 0, "Falta material 1");
@@ -63,7 +73,7 @@ public class ObservacionTest {
     }
     
     @Test
-    public void testObs03A(){
+    public void testOBS01D(){
         boolean assertError=false;
         try{
         Observacion aux= new Observacion(Observacion.TEMA_INSUMOS, 1, null);
@@ -76,7 +86,7 @@ public class ObservacionTest {
     }
     
     @Test
-    public void testObs04A(){
+    public void testOBS01E(){
         boolean assertError=false;
         try{
         Observacion aux= new Observacion(Observacion.TEMA_INSUMOS, 1,"");
@@ -89,7 +99,7 @@ public class ObservacionTest {
     }
     
     @Test
-    public void testObs05A(){
+    public void testOBSO1F(){
         boolean assertError=false;
         try{
         Observacion aux= new Observacion(null, 1,"Falta material 1");
@@ -102,7 +112,7 @@ public class ObservacionTest {
     }
     
     @Test
-    public void testObs06A(){
+    public void testOBS01G(){
         boolean assertError=false;
         try{
         Observacion aux= new Observacion("", 1,"Falta material 1");
@@ -115,7 +125,7 @@ public class ObservacionTest {
     }
     
     @Test
-    public void testObs07A(){
+    public void testOBS01H(){
         boolean assertError=false;
         try{
         Observacion aux= new Observacion("COMPRAS", 1,"Falta material 1");
@@ -128,26 +138,138 @@ public class ObservacionTest {
     }
     
     @Test
-    public void testObs08A(){
-        Observacion o= new Observacion(Observacion.TEMA_INSUMOS, 1,"Falta material 1");
-        int iguales= fixture.getObservacionTest().compareTo(o);
-        assertTrue("Ambas observaciones deberian ser iguales", iguales==0);
+    public void testOBS02A(){
+        fixture.setUpM02A();   
+        Calendar fechaAux = new GregorianCalendar(2016, 12, 12);
+        resetHoraFecha(fechaAux);
+        Observacion o = new Observacion(Observacion.TEMA_OTROS, 1, "Prueba");
+        o.setFecha(fechaAux);
+        int cmp = fixture.getObservacionTest().compareTo(o);
+        assertTrue("OBS02A: Comparacion incorrecta.", cmp < 0);
+    }
+    
+    @Test 
+    public void testOBS02B(){
+        fixture.setUpM02B();   
+        Calendar fechaAux = new GregorianCalendar(2016, 10, 12);
+        resetHoraFecha(fechaAux);
+        Observacion o = new Observacion(Observacion.TEMA_OTROS, 1, "Prueba");
+        o.setFecha(fechaAux);
+        int cmp = fixture.getObservacionTest().compareTo(o);
+        assertTrue("OBS02B: Comparacion incorrecta.", cmp < 0);
     }
     
     @Test
-    public void testObs09A(){
-        boolean assertError=false;
-        try{
-        int iguales= fixture.getObservacionTest().compareTo(null);
-        }
-        catch(AssertionError e){
-            assertError=true;
-        }
-        if (!assertError)
-            fail("Se esperaba un error de asercion");
+    public void testOBS02C(){
+        fixture.setUpM02A();   
+        Calendar fechaAux = new GregorianCalendar(2016, 10, 12);
+        resetHoraFecha(fechaAux);
+        Observacion o = new Observacion(Observacion.TEMA_OTROS, 1, "Prueba");
+        o.setFecha(fechaAux);
+        int cmp = fixture.getObservacionTest().compareTo(o);
+        assertTrue("OBS02C: Comparacion incorrecta.", cmp < 0);
     }
     
+    @Test
+    public void testOBS02D(){
+        fixture.setUpM02D();
+        Calendar fechaAux = new GregorianCalendar(2016, 12, 12);
+        resetHoraFecha(fechaAux);
+        Observacion o = new Observacion(Observacion.TEMA_INSUMOS, 1, "Prueba");
+        o.setFecha(fechaAux);
+        int cmp = fixture.getObservacionTest().compareTo(o);
+        assertTrue("OBS02D: Comparacion incorrecta.", cmp > 0);
+    }
     
+    @Test
+    public void testOBS02E(){
+        fixture.setUpM02E();
+        Calendar fechaAux = new GregorianCalendar(2016, 10, 12);
+        resetHoraFecha(fechaAux);
+        Observacion o = new Observacion(Observacion.TEMA_INSUMOS, 1, "Prueba");
+        o.setFecha(fechaAux);
+        int cmp = fixture.getObservacionTest().compareTo(o);
+        assertTrue("OBS02E: Comparacion incorrecta.", cmp > 0);
+    }
     
+    @Test
+    public void testOBS02F(){
+        fixture.setUpM02D();
+        Calendar fechaAux = new GregorianCalendar(2016, 10, 12);
+        resetHoraFecha(fechaAux);
+        Observacion o = new Observacion(Observacion.TEMA_INSUMOS, 1, "Prueba");
+        o.setFecha(fechaAux);
+        int cmp = fixture.getObservacionTest().compareTo(o);
+        assertTrue("OBS02F: Comparacion incorrecta.", cmp > 0);
+    }
+    
+    @Test
+    public void testOBS02G(){
+        fixture.setUpM02A();
+        Calendar fechaAux = new GregorianCalendar(2016, 12, 12);
+        resetHoraFecha(fechaAux);
+        Observacion o = new Observacion(Observacion.TEMA_INSUMOS, 1, "Prueba");
+        o.setFecha(fechaAux);
+        int cmp = fixture.getObservacionTest().compareTo(o);
+        assertTrue("OBS02G: Comparacion incorrecta.", cmp < 0);
+    }
+    
+    @Test
+    public void testOBS02H(){
+        fixture.setUpM02A();
+        Calendar fechaAux = new GregorianCalendar(2016, 10, 13);
+        resetHoraFecha(fechaAux);
+        Observacion o = new Observacion(Observacion.TEMA_INSUMOS, 1, "Prueba");
+        o.setFecha(fechaAux);
+        int cmp = fixture.getObservacionTest().compareTo(o);
+        assertTrue("OBS02H: Comparacion incorrecta.", cmp < 0);
+    }
+    
+    @Test
+    public void testOBS02I(){
+        fixture.setUpM02B();
+        Calendar fechaAux = new GregorianCalendar(2016, 12, 12);
+        resetHoraFecha(fechaAux);
+        Observacion o = new Observacion(Observacion.TEMA_INSUMOS, 1, "Prueba");
+        o.setFecha(fechaAux);
+        int cmp = fixture.getObservacionTest().compareTo(o);
+        assertTrue("OBS02I: Comparacion incorrecta.", cmp == 0);
+    }
+    
+    @Test
+    public void testOBS02J(){
+        fixture.setUpM02B();
+        Calendar fechaAux = new GregorianCalendar(2016, 10, 12);
+        resetHoraFecha(fechaAux);
+        Observacion o = new Observacion(Observacion.TEMA_INSUMOS, 1, "Prueba");
+        o.setFecha(fechaAux);
+        int cmp = fixture.getObservacionTest().compareTo(o);
+        assertTrue("OBS02J: Comparacion incorrecta.", cmp > 0);
+    }
+    
+    @Test
+    public void testOBS02K(){
+        fixture.setUpM02B();
+        Calendar fechaAux = new GregorianCalendar(2016, 12, 11);
+        resetHoraFecha(fechaAux);
+        Observacion o = new Observacion(Observacion.TEMA_INSUMOS, 1, "Prueba");
+        o.setFecha(fechaAux);
+        int cmp = fixture.getObservacionTest().compareTo(o);
+        assertTrue("OBS02K: Comparacion incorrecta.", cmp > 0);
+    }
+    
+    @Test
+    public void testOBS02L(){
+        fixture.setUpM02A();
+        int cmp = fixture.getObservacionTest().compareTo(null);
+        assertTrue("OBS02L: Comparacion incorrecta.", cmp == 0);
+    }
+    
+    @Test
+    public void testOBS02M(){
+        fixture.setUpM02A();
+        int cmp = fixture.getObservacionTest().compareTo("Ponenos 10");
+        assertTrue("OBS02M: Comparacion incorrecta.", cmp == 0);
+    }
     
 }
